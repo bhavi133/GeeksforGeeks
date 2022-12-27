d8bc52e7b18c6edd839be3059114d24431047fa8
Bhavesh Lulla <65904865+bhavi133@users.noreply.github.com>
2022-12-27 20:17:14 +0530

    Create Largest Element in Array.py

---
+++ Basic/Largest Element in Array.py
@@ -0,0 +1,23 @@
+Link : https://practice.geeksforgeeks.org/problems/largest-element-in-array4009/1?page=1&difficulty[]=-1&status[]=solved&sortBy=submissions
+
+Code :
+
+#User function Template for python3
+
+def largest( arr, n):
+    return max(arr)
+
+
+# Driver Code Starts
+# Initial Template for Python 3
+def main():
+    T = int(input())
+    while(T > 0):
+        n = int(input())
+        a = [int(x) for x in input().strip().split()]
+        print(largest(a, n))
+        T -= 1
+
+if __name__ == "__main__":
+    main()
+# Driver Code Ends
