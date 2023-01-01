f2fc9c48be422ecfd37b7d25c7ebcefdc057e9fa
Bhavesh Lulla <65904865+bhavi133@users.noreply.github.com>
2023-01-01 20:28:45 +0530

    Create Armstrong Number.py

---
+++ School/Armstrong Number.py
@@ -0,0 +1,29 @@
+Link : https://practice.geeksforgeeks.org/problems/armstrong-numbers2727/1?page=1&difficulty[]=-2&status[]=solved&sortBy=submissions
+
+Code :
+
+#User function Template for python3
+
+import math
+
+# User function Template for python3
+class Solution:
+    def armstrongNumber (ob, n):
+        l1 = list(map(int, str(n)))
+        l2 = list(map(lambda x:x**3, l1))
+        if(sum(l2) == n):
+            return 'Yes'
+        else:
+            return 'No'
+
+
+# Driver Code Starts
+# Initial Template for Python 3
+if __name__ == '__main__': 
+    t = int (input ())
+    for _ in range (t):
+        n = input()
+        n=int(n)
+        ob = Solution()
+        print(ob.armstrongNumber(n))
+# Driver Code Ends
