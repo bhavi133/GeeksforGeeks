296decbc239845e32cb4fc8afcb537f4aa3f26f2
Bhavesh Lulla <65904865+bhavi133@users.noreply.github.com>
2023-02-14 13:56:24 +0530

    Create Maximum no of 1's row.py

---
+++ Easy/Maximum no of 1's row.py
@@ -0,0 +1,30 @@
+Link : https://practice.geeksforgeeks.org/problems/maximum-no-of-1s-row3027/1?page=2&status[]=unsolved&curated[]=1&sortBy=submissions
+
+Code :
+
+# User function Template for python3
+
+class Solution:
+    def maxOnes (self, Mat, N, M):
+        return Mat.index(max(Mat))
+
+
+
+# Driver Code Starts
+# Initial Template for Python 3
+
+if __name__ == '__main__': 
+    t = int(input())
+    for _ in range(t):
+        size = input().strip().split()
+        r = int(size[0])
+		c = int(size[1])
+		line = list(map(int,input().split()))
+		matrix = [ [0 for _ in range(c)] for _ in range(r) ]
+        for i in range(r):
+			for j in range(c):
+				matrix[i][j] = line[i*c+j]
+        ob = Solution()
+        print(ob.maxOnes(matrix,r,c))
+
+# Driver Code Ends
