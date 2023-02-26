b5c60bd134f560381757e6e297c90179fff4d1c5
Bhavesh Lulla <65904865+bhavi133@users.noreply.github.com>
2023-02-26 20:42:55 +0530

    Create Nth node from end of linked list.py

---
+++ Easy/Nth node from end of linked list.py
@@ -0,0 +1,69 @@
+Link : https://practice.geeksforgeeks.org/problems/nth-node-from-end-of-linked-list/1?page=1&status[]=bookmarked&sortBy=submissions
+
+Code :
+
+def getNthFromLast(head,n):
+    l = []
+    if head is None:
+        return 
+    else:
+        while head is not None:
+            l.append(head.data)
+            head = head.next
+    
+    for i in l:
+        if len(l) >= n:
+            return l[-n]
+    else:
+        return -1
+
+
+# Driver Code Starts
+# Initial Template for Python 3
+
+import atexit
+import io
+import sys
+
+_INPUT_LINES = sys.stdin.read().splitlines()
+input = iter(_INPUT_LINES).__next__
+_OUTPUT_BUFFER = io.StringIO()
+sys.stdout = _OUTPUT_BUFFER
+
+@atexit.register
+
+def write():
+    sys.__stdout__.write(_OUTPUT_BUFFER.getvalue())
+    
+# Node Class
+class Node:
+    def __init__(self, data):   # data -> value stored in node
+        self.data = data
+        self.next = None
+
+# Linked List Class
+class LinkedList:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+
+    # creates a new node with given value and appends it at the end of the linked list
+    def append(self, new_value):
+        new_node = Node(new_value)
+        if self.head is None:
+            self.head = new_node
+            self.tail = new_node
+            return
+        self.tail.next = new_node
+        self.tail = new_node
+
+if __name__ == '__main__':
+    t=int(input())
+    for cases in range(t):
+        n,nth_node = map(int, input().strip().split())
+        a = LinkedList() # create a new linked list 'a'.
+        nodes_a = list(map(int, input().strip().split()))
+        for x in nodes_a:
+            a.append(x)  # add to the end of the list
+        print(getNthFromLast(a.head,nth_node))
+# Driver Code Ends
