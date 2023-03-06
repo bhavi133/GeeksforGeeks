7e1c77f18addf1399d351f4c77b3511a6527505b
Bhavesh Lulla <65904865+bhavi133@users.noreply.github.com>
2023-03-06 12:30:16 +0530

    Create Pattern 3.py

---
+++ Easy/Pattern 3.py
@@ -0,0 +1,25 @@
+Link : https://practice.geeksforgeeks.org/problems/triangle-number/1?utm_source=youtube&utm_medium=collab_striver_ytdescription&utm_campaign=pattern_3
+
+Code :
+
+# User function Template for python3
+
+class Solution:
+    def printTriangle(self, N):
+        for i in range(1, N+1):
+            for j in range(1, i+1):
+                print(j, end=" ")
+            print()
+
+
+# Driver Code Starts
+# Initial Template for Python 3
+
+if __name__ == '__main__':
+    t = int(input())
+    for _ in range(t):
+        N = int(input())
+        ob = Solution()
+        ob.printTriangle(N)
+
+# Driver Code Ends
