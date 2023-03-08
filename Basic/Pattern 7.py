ced2cd3c78587ec2140682734aff24130ac9478a
Bhavesh Lulla <65904865+bhavi133@users.noreply.github.com>
2023-03-08 12:40:41 +0530

    Create Pattern 7.py

---
+++ Basic/Pattern 7.py
@@ -0,0 +1,30 @@
+Link : https://practice.geeksforgeeks.org/problems/triangle-pattern-1661492263/1?utm_source=youtube&utm_medium=collab_striver_ytdescription&utm_campaign=pattern_7
+
+Code :
+
+# User function Template for python3
+
+class Solution:
+    def printTriangle(self, N):
+        # Code here
+        for i in range(1, N+1):
+            for j in range(1, N-i+1):
+                print(" ", end="")
+            
+            for j in range(1, 2*i-1+1):
+                print('*', end="")
+            
+            print()
+            
+
+# Driver Code Starts
+# Initial Template for Python 3
+
+if __name__ == '__main__':
+    t = int(input())
+    for _ in range(t):
+        N = int(input())
+        ob = Solution()
+        ob.printTriangle(N)
+
+# Driver Code Ends
