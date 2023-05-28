91bdd415652b7f1d0971d1a527207b470f9fce8d
Bhavesh Lulla <65904865+bhavi133@users.noreply.github.com>
2023-05-28 13:54:46 +0530

    Update Binary representation of next number.py

--- Basic/Binary representation of next number.py
+++ Basic/Binary representation of next number.py
@@ -3,22 +3,22 @@ Link : https://practice.geeksforgeeks.org/problems/binary-representation-of-next
 Code :
 
 class Solution:
-	def binaryNextNumber(self, S):
-	    # x = int(S, 2)
-      # y = bin(x+1)[2:]
-      # return y
-        
-      return bin(int(S, 2)+1)[2:]
+    def binaryNextNumber(self, S):
+	# x = int(S, 2)
+        # y = bin(x+1)[2:]
+        # return y
+	
+	return bin(int(S, 2)+1)[2:]
 
 
 # Driver Code Starts
 
 if __name__ == '__main__':
-	T=int(input())
-	for i in range(T):
-		S = input()
-		ob = Solution();
-		ans = ob.binaryNextNumber(S)
-		print(ans)
+    T=int(input())
+    for i in range(T):
+	S = input()
+	ob = Solution();
+	ans = ob.binaryNextNumber(S)
+	print(ans)
 
 # Driver Code Ends
